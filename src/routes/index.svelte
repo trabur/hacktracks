2aa47f37d57884c3b28accc28e12abce610622fe
trabur <travis.burandt@gmal.com>
2020-03-11 18:33:54 -0500

    From Ideas.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 	<div style="box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12); background: url('/master-chief.jpg'); height: 500px; background-position: top center; background-size: cover;"></div>
 	<div style="max-width: 900px; margin: -200px auto 5em;">
 		<h1 style="font-weight: 700; margin: 0; text-shadow: 0 0 0.4em #000; color: #eee; font-size: 2em; padding: 0; line-height: 2em; text-align: left; text-transform: none;">
-			From Idea. To Checkpoints. With Forge.
+			From Ideas. To Checkpoints. With Forge.
 		</h1>
 		<Paper elevation={5} style="background: #111; color: #ccc;">
 			<Title>Create account:</Title>
