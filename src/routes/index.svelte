e8db16f61134c8aa0b642a9c753dd49850da1282
trabur <travis.burandt@gmal.com>
2020-03-12 02:47:13 -0500

    #hacktracks.org

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,16 +15,22 @@
 			<Content>
 				<p>Painful or enjoyable... Trash or artistic... HT is about (forge) {checkpoint}. To begin, let's back trace our steps to explain what is going on here...</p>
 				<p>The culture around HT is as simple as mud. Think about family trees for a moment. Grandparents raise parents, parents raise children, children raise grandchildren, and so on. To best represent this data structure we can think of an "alphabet" where letter "A" makes rules for letter "B", letter "B" makes rules for letter "C", and so on. A culture deeply rooted in supervisor trees.</p>
-				<p>The officiating around HT is somewhat complicated. Measuring youself before a race is good training but isn't the real thing. Measuring youself durring a race may yeild better results. Try a simple math situation like 1 + 1: (The solution to this problem (1 + 1 = 2) can be solved in many different ways. You could have used a calculator. You could have looked back at what has already been said.) However you got it, when it comes to making rules, sometimes the best measurment is post race: [2].</p>
-				<p>The spirit around HT is about riding to win the race. Let's run a mock event, for demonstration purposes, with the above culture and officiating ideals. As riders roll up to the starting gate "officials" will pin numbers onto the racers like variables in a program such that A -> 1, B -> 2, C -> 3, and so on...</p>
-				<p>The fassion around HT is procedural; moving from checkpoint to checkpoint. Having acknologed: "culture around HT" to "officiating around HT" to "spirit around HT" to... is like making a statement. While these words may be the shortest path to getting the point across -> these are just guidelines = culture, officiating, spirit, etc... what you choose to download and play on your own xbox is totally up to you. With all of that said ... have fun and please feel free to copy, change, or distribute ... (:</p>
+				<p>The officiating around HT is somewhat complicated. Measuring youself before a race is good training but isn't the real thing. Measuring youself durring a race may yeild better results. Try a simple math situation like 1 + 1: (The solution to this problem {math} can be solved in many different ways. You could have used a calculator. You could have looked back at what has already been said.) However you got it, when it comes to making rules, sometimes the best measurment can only be taken post race: [2].</p>
+				<p>The spirit around HT is about riding to win the race. Let's run a mock event, for demonstration purposes, with the above culture and officiating ideals. As riders roll up to the starting gate "officials" will pin numbers onto the racers like variables in a program such that A -> 1, B --> 2, C ---> 3, and so on...</p>
+				<p>The fassion around HT is procedural; moving from checkpoint to checkpoint. Having acknologed: "culture around HT" to "officiating around HT" to "spirit around HT" to... is like making an chain of blocks. While these letters may be the shortest path to getting the point across >>> HT ideals are just guidelines = culture, officiating, spirit, etc... what you choose to download and play on your own xbox is totally up to you. With all of that said ... have fun and please feel free to copy, change, or distribute ... (:</p>
 				<p>The cheating around HT is an ongoing issue. This is a position where a rider performs harm in order to gain an unfair advantage. The only arguement for holding this line is directly under fassion where a (rider) {hat}; such that they may understand the true nature of the beast. The cheaters position is obviously a line that can't be held for a very long time due to it's self destructive behavior with parasitic intent.</p>
 				<p>The history around HT started in 2007 as a bungie.net group and then into halotracks.org. The ^Group Founder, "C U L8er" had left for many years and now returns as "d minus c" for who knows why... As the author of hacktracks.org my plan is to have a resting place for cu's gametypes & maps. Consider this website/canvas a playground for crazy ideas and wild jump statements to form ---> :)</p>
 			</Content>
 			<Title>ACKISM:</Title>
 			<Content>
-				<p>M = ACK. The thing about cycling or going round and round a track is that you have options. Most riders may just be riding a {push} as if they are reading (left to right) ---> (top to bottom) ---> [Z]. There is also the {clipped} where riders may push down --->) and ({up} pull up while [Z]. Experienced riders, depending on the fassion, could be making )( moves on not just sentences but also words, paragraphs, ideas, etc...</p>
+				<p>M = ACK. The thing about cycling or going round and round a track is that you have options. Most riders may just be riding a {push} as if they are reading (left to right) ---> (top to bottom) ---> [Z]. There is also the {clipped} where riders may push down --->) and ({up} pull up while [Z]. Experienced riders, depending on the fassion, could be making )( moves not just on sentences but also words, paragraphs, ideas, etc...</p>
 				<p>The term "ackism" is used because it can easily be found by search engines. The term "racism" (not to be mistaken) has bagage attached to it differently than what HT is trying to accomplish; sacred hyroglyphs with forgeable blocks.</p>
+				<p>Other racing organizations, such as uci.org: the *governing* body of cycling around the world, has made attempts time and time again to destroy our belovid sport. Their false start & end lines, bogus mile markers, fake trophies/medals, droned out spectators, makes for a graffiti sestpool. You don't fool us: {up}ICU.</p>
+				<p>This brings us full circle back to, "Ready? Set. Go!" ... the system in place can work with you like "ART" and make you "HAHA" or you can build step pyramids, climb ladders, and be stuck in constant "TRASH" screaming "AHHH"; this is the ERA that we live in. Mention ackism to me on discord or anywhere else and I'd be happy to contune the discussion {up}THere.</p>
+			</Content>
+			<Title>#hacktracks.org</Title>
+			<Content>
+
 			</Content>
 		</Paper>
 	</div>
@@ -45,4 +51,5 @@
 	let push = "{ push pedal bike }";
 	let clipped = "{ clipped pedal bike }";
 	let up = "<---";
+	let math = "{ 1 + 1 = 2 }";
 </script>
