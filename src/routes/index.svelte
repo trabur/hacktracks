b905f6b1d09a39971e8640aac49a9aa32b447899
trabur <travis.burandt@gmal.com>
2020-03-12 00:21:59 -0500

    ---> :]

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,28 +11,19 @@
 			From Ideas. To Checkpoints. With Forge.
 		</h1>
 		<Paper elevation={5} style="background: #111; color: #ccc;">
-			<Title>Create account:</Title>
+			<Title>Ready? Set. Go!</Title>
 			<Content>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
-				<p>Testing... </p>
+				<p>Painful or enjoyable... Trash or artistic... HT is about (forge) {checkpoint}. To begin, let's back trace our steps to explain what is going on here...</p>
+				<p>The culture around HT is as simple as mud. Think about family trees for a moment. Grandparents raise parents, parents raise children, children raise grandchildren, and so on. To best represent this data structure we can think of an "alphabet" where letter "A" makes rules for letter "B", letter "B" makes rules for letter "C", and so on. A culture deeply rooted in supervisor trees.</p>
+				<p>The officiating around HT is somewhat complicated. Measuring youself before a race is good training but isn't the real thing. Measuring youself durring a race may yeild better results. Try a simple math situation like 1 + 1: (The solution to this problem (1 + 1 = 2) can be solved in many different ways. You could have used a calculator. You could have looked back at what has already been said.) However you got it, when it comes to making rules, sometimes the best measurment is post race: [2].</p>
+				<p>The spirit around HT is about riding to win the race. Let's run a mock event, for demonstration purposes, with the above culture and officiating ideals. As riders roll up to the starting gate "officials" will pin numbers onto the racers like variables in a program such that A -> 1, B -> 2, C -> 3, and so on...</p>
+				<p>The fassion around HT is procedural; moving from checkpoint to checkpoint. Having acknologed: "culture around HT" to "officiating around HT" to "spirit around HT" to... is like making a statement. While these words may be the shortest path to getting the point across -> these are just guidelines = culture, officiating, spirit, etc... what you choose to download and play on your own xbox is totally up to you. With all of that said ... have fun and please feel free to copy, change, or distribute ... :)</p>
+				<p>The cheating around HT is an ongoing issue. This is a position where a rider performs harm in order to gain an unfair advantage. The only arguement for holding this line is directly under fassion where a (rider) {hat}; such that they may understand the true nature of the beast. The cheaters position is obviously a line that can't be held for a very long time due to it's self destructive behavior with parasitic intent.</p>
+				<p>The history around HT started in 2007 as a bungie.net group and then into halotracks.org. The ^Group Founder, "C U L8er" had left for many years and now returns as "d minus c" for who knows why... As the author of hacktracks.org my goal is to have a resting place for cu's maps. Consider this website/canvas a playground for crazy ideas and wild jump statements to form ---> :]</p>
+			</Content>
+			<Title>ACKISM:</Title>
+			<Content>
+				<p>The term "ackism" is used because it can easily be found by search engines. The term "racism" (not to be mistaken) has bagage attached to it differently than what HT is trying to accomplish; sacred hyroglyphs with forgeable blocks.</p>
 			</Content>
 		</Paper>
 	</div>
@@ -43,5 +34,8 @@
 	import { onMount, onDestroy } from 'svelte';
   import Paper, {Title, Subtitle, Content} from '@smui/paper';
   import {Label, Icon} from '@smui/common';
-  import {mdiConsoleLine} from '@mdi/js';
+	import {mdiConsoleLine} from '@mdi/js';
+	
+	let checkpoint = "{ acknologing checkpoints for competative enviornments }";
+	let hat = "{ puts on a sort of cheaters hat }";
 </script>
