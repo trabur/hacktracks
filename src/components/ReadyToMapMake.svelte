b6770e42ef43a43a88e76dda36b9ac73262ce89c
trabur <travis.burandt@gmal.com>
2020-03-22 01:05:53 -0500

    footer spacing

--- src/components/ReadyToMapMake.svelte
+++ src/components/ReadyToMapMake.svelte
@@ -1,12 +1,12 @@
 
-<div class="mobile only tablet only" style='padding: 0 1em; margin: 1em auto; border-top: 3px solid #111;'>
+<div class="mobile only tablet only" style='padding: 0 1em; margin: 0em auto 5em; border-top: 3px solid #111;'>
   <h2 style="color: #ccc; font-size: 1em; font-weight: 500; margin: 0;">Interested in becoming Map Maker?</h2>
   <p style="color: #ccc; margin: -1em 0;">GOTO: our discord then under #map-makers read the pinned topic.</p>
   <br />
   <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='https://discord.gg/HXPq4d' target='_blank' variant="unelevated" color='secondary'>JOIN</Button>
 </div>
 
-<div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 1em auto; border-top: 3px solid #111;'>
+<div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 0em auto 5em; border-top: 3px solid #111;'>
   <Button style="float: right; margin: 1em 0; height: 3.6em;" href='https://discord.gg/HXPq4d' target='_blank' variant="unelevated" color='secondary'>JOIN</Button>
   <h2 style="color: #ccc; font-size: 1.5em; font-weight: 500; margin: 0;">Interested in becoming Map Maker?</h2>
   <p style="color: #ccc; margin: -1em 0;">GOTO: our discord then under #map-makers read the pinned topic.</p>
