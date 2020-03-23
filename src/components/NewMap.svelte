52df52074e2cc48202dca0ad655357bcaebd14c7
trabur <travis.burandt@gmal.com>
2020-03-22 22:03:04 -0500

    layout

--- src/components/NewMap.svelte
+++ src/components/NewMap.svelte
@@ -1,14 +1,9 @@
 
 {#if account === username}
-  <div class="mobile only tablet only" style='padding: 0 1em; margin: 1em auto; border-top: 3px solid #111;'>
+  <div class="mobile only tablet only" style='padding: 0 1em; margin: 1em auto 5em; border-top: 3px solid #111;'>
     <Button style="width: 100%; margin: 1em 0; height: 3.6em;" on:click={() => createDialog.open()} variant="unelevated" color='secondary'>ADD MAP</Button>
   </div>
-{:else}
-  <!---->
-{/if}
-
-{#if account === username}
-  <div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 1em auto; border-top: 3px solid #111;'>
+  <div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 1em auto 5em; border-top: 3px solid #111;'>
     <Button style="float: right; margin: 1em 0; height: 3.6em;" on:click={() => createDialog.open()} variant="unelevated" color='secondary'>ADD MAP</Button>
   </div>
 {:else}
