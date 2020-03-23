7d169dc5a7019906325e0532001a2ceaf150815a
trabur <travis.burandt@gmal.com>
2020-03-23 18:59:31 -0500

    TAC

--- src/routes/privacy-policy.svelte
+++ src/routes/privacy-policy.svelte
@@ -27,7 +27,7 @@
       If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
     </p>
     <p>
-      The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at /terms-and-conditions, unless otherwise defined in this Privacy Policy.
+      The terms used in this Privacy Policy have the same meanings as in our Terms & Conditions, which is accessible at /terms-and-conditions, unless otherwise defined in this Privacy Policy.
     </p>
     <h5>
       Information Collection and Use
