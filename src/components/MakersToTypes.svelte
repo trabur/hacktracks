9990cf4115d899fbfe0b6d5cdab1b3050ae58ba8
trabur <travis.burandt@gmal.com>
2020-03-23 22:06:57 -0500

    #@^

--- src/components/MakersToTypes.svelte
+++ src/components/MakersToTypes.svelte
@@ -1,12 +1,12 @@
 
 <div class="mobile only tablet only" style='padding: 0 1em; margin: 0em auto 5em; border-top: 3px solid #111;'>
-  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/map-makers' variant="unelevated" color='secondary'>MAP MAKERS</Button>
+  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/library' variant="unelevated" color='secondary'>THE LIBRARY</Button>
   <br />
   <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/race-gametypes' variant="unelevated" color='secondary'>RACE GAMETYPES</Button>
 </div>
 
 <div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 0em auto 5em; border-top: 3px solid #111;'>
-  <Button style="margin: 1em 0; height: 3.6em;" href='/map-makers' variant="unelevated" color='secondary'>MAP MAKERS</Button>
+  <Button style="margin: 1em 0; height: 3.6em;" href='/library' variant="unelevated" color='secondary'>THE LIBRARY</Button>
   <Button style="float: right; margin: 1em 0; height: 3.6em;" href='/race-gametypes' variant="unelevated" color='secondary'>RACE GAMETYPES</Button>
 </div>
 
