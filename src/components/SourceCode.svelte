eb11411e96b73bc6ef6ef14a043a18c81c673f17
trabur <travis.burandt@gmal.com>
2020-03-26 20:04:12 -0500

    open-source

---
+++ src/components/SourceCode.svelte
@@ -0,0 +1,36 @@
+
+{#if open}
+  <Paper elevation={5} style="background: #111; color: #ccc;">
+    <Title>Ready? Set. Go!</Title>
+    <Content>
+      <p>Hilarious or hurtful... Trash or treasure... HT is about (race) {race}; to begin, let's back trace our steps to explain what is going on here...</p>
+      <p>The culture around HT is as simple as mud. Think about family trees for a moment. Grandparents raise parents, parents raise children, children raise grandchildren, and so on. To best represent this data structure we can use an "alphabet" where letter "A" makes rules for letter "B", letter "B" makes rules for letter "C", and so on. A culture deeply rooted in supervisor trees.</p>
+      <p>The officiating around HT is somewhat complicated. Measuring yourself before a race is good training but isn't the real thing. Measuring yourself durring a race may yield better results. Try a simple math situation like 1 + 1: (The solution to this problem {math} can be solved in many different ways. You could have used a calculator. You could have looked back at what has already been said.) However you got it, when it comes to making rules, sometimes the best measurment can only be taken post race: [2].</p>
+      <p>The spirit around HT is about riding to win the race. Let's run a mock event, for demonstration purposes, with the above culture and officiating ideals. As riders roll up to the starting gate "officials" will pin numbers onto the racers like variables in a program such that A -> 1, B --> 2, C ---> 3, and so on...</p>
+      <p>The fassion around HT is procedural; moving from checkpoint to checkpoint. Having acknologed: "culture around HT" to "officiating around HT" to "spirit around HT" to... is like making a chain of blocks for others to catch draft. While these letters may be the shortest path to getting the point across >>> HT ideals are just guidelines = culture, officiating, spirit, etc... what you choose to download and play on your own xbox is totally up to you. With all of that said ... have fun and please feel free to copy, change, or distribute ... (:</p>
+      <p>The cheating around HT is an-ongoing-issue. This is a position where a rider performs harm in order to gain an unfair advantage. The only arguement for holding this line is directly under fassion where a (rider) {hat}; such that they may understand the true nature of the beast. The cheaters position is obviously a line that can't be held for a very long time due to it's self destructive behavior with parasitic-intent.</p>
+      <p>The history around HT started in 2007 as a bungie.net group and then into halotracks.org. The ^Group Founder, "c u l8er" had left for many years and now returns as "d minus c" for who knows why... As the author of hacktracks.org my plan is to have a resting place for cu's gametypes & maps. Consider this website/canvas a playground for crazy ideas and wild jump statements to form. ---> :)</p>
+    </Content>
+    <Title>ACKISM:</Title>
+    <Content>
+      <p>M = ACK. The thing about cycling or going round and round a track is that you have options. Most riders may just be riding a {push} as if they are reading (left to right) ---> (top to bottom) ---> [Z]. There is also the {clipped} where riders may push down --->) and ({up} pull up while [Z]. Experienced riders, depending on the fassion, could be making )( moves not just on sentences but also words, paragraphs, ideas, etc...</p>
+      <p>The term "ackism" is used because it can easily be found by search engines. The term "racism" (not to be mistaken) has bagage attached to it differently than what HT is trying to accomplish; sacred hyroglyphs with forgeable blocks.</p>
+      <p>Other racing organizations, such as uci.org: the international cycling union *governing* body, has made attempts time and time again to destroy our belovid sport. Their false start & end lines, bogus mile markers, fake trophies/medals, and droned out spectators makes for a sponsorship graffiti sestpool :| ICU |: eye see you.</p>
+      <p>Cycling also has the concept of single pace lines and double pace lines. If I where to pull charaters from the keyboard to best represent these pace lines then it would be --- and === respectively; do you see one group of three riders off the front (in a single pace line) and a pack behind them of six riders (in a double pace line)? </p>
+      <p>Slash. Slash. Slash. Let's say the wind direction changes (: then we have our single pace line riding like this / or \ and our double pace line riding like this // or \\ riders will then rotate clockwise or counter clockwise :) ... as a group it is better to rotate into the wind, so that as the leading rider peals off, draft is provided for the advancing riders. One could say controllers obay the same fluid dynamic laws.</p>
+      <p>What's fascinating about tournaments or events in general such as the "Tour de France" is the organization around traffic, routing, and safety. One thing in particular is the race caravan that follows the riders (: flagged as three dots everywhere in this script :) because the event travels over such vast distances officials on motorcycles are consrantly trying to block:traffic, flag+hazards, keep-it-safe, free=road=traffic... while repeating again and again until the race is over.</p>
+      <p>Forging maps from scratch sort of follows the same mechanics of running events like the TDF; WallRider, for instance, took two forgers a month to create from a blank canvas. It was the first map in H3 to feature a starting g:a:t:e, off-camber-turns, inter=locking=blocks, and a shield+door+elevator. Maps like these may first just start out as a simple turn but when multiple people start to join in game then turns, ramps, jumps, landings, etc... become more and more fun, "{fun}".</p>
+      <p>This brings us full circle back to, "Ready? Set. Go!" ... the system in place can work with you like "ART" and make you "HAHA" or you can build step pyramids [BTC, ETC], climb ladders [H], and be stuck in constant "TRASH" screaming "AHHH"; because crashing hurts don't be a cheating china! Rather, keep caution in mind --> smiles: it is what it is = the (E(R)A) that we live in. Ping me about ackism on discord or anywhere else and I'd be happy to continue the discussion {up}THere.</p>
+    </Content>
+    <Title>#hacktracks.org</Title>
+    <Content>
+      <!--- <p>Brace yourself for story mode.</p> --->
+    </Content>
+  </Paper>
+{/if}
+
+<script>
+  import Paper, {Title, Subtitle, Content} from '@smui/paper';
+
+  let open = false;
+</script>
