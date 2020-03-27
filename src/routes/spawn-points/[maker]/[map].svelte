e7bb87bdfdd09251544a15d39803dee76abdb526
trabur <travis.burandt@gmal.com>
2020-03-26 22:01:30 -0500

    map layout

--- src/routes/spawn-points/[maker]/[map].svelte
+++ src/routes/spawn-points/[maker]/[map].svelte
@@ -12,11 +12,36 @@
 <div style="height: 100%; width: 100%;">
 	<div style="margin-bottom: -100px; box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12); background: url('/hauntedrider.png'); height: 500px; background-position: top center; background-size: cover;"></div>
 	<div style="max-width: 900px; margin: 0 auto 5em;">
-		<Paper elevation={5} style="background: #111; color: #ccc; padding: 1em; min-height: 600px;">
-			<Title>{map}<span style="color: #aaa; float: right; font-size: 0.8em;">:)kudos [3]</span></Title>
-			<Content>
-				<p>map list here...</p>
-			</Content>
+		<Paper elevation={5} style="color: #111; padding: 1em;">
+			<div>
+				<Title>
+					<img src={`avatars/map-maker-avatar.png`} alt={'#map'} class="mobile hidden" style="margin-right: 1em; margin-top: -3em; float: left; width: 100px;" />
+					<span>{map}</span>
+					<Button class="mobile hidden" color="secondary" variant="unelevated" style='float: right;'>
+						<Label>DOWNLOAD</Label>
+					</Button>
+					<Button class="mobile only" color="secondary" variant="unelevated" style='width: 100%; margin-top: 1em;'>
+						<Label>DOWNLOAD</Label>
+					</Button>
+				</Title>
+				<br />
+				<TabBar tabs={navigation} let:tab bind:active style='margin: 0px;'>
+					<Tab {tab} minWidth>
+						{#if tab === 'README.md'}
+							<Label>{tab}</Label>
+						{/if}
+						{#if tab === ':)Kudos'}
+							<Label>{tab} [{kudosCount}]</Label>
+						{/if}
+					</Tab>
+				</TabBar>
+			</div>
+
+			<div style="color: #ccc; background: #111; margin: 0 -1em -1em; min-height: 600px; border-radius: 0 0 4px 4px;">
+				<Content style="padding: 1em;">
+					<p>description content etc... description content etc... description content etc...</p>
+				</Content>
+			</div>
 		</Paper>
 		<br />
 		<MakersToTypes />
@@ -25,11 +50,16 @@
 
 <script>
 	import { onMount } from 'svelte';
+  import Button, {Group, GroupItem, Icon} from '@smui/button';
+  import Tab, {Label} from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
 	import Paper, {Title, Subtitle, Content} from '@smui/paper';
 	import MakersToTypes from '../../../components/MakersToTypes';
 
+  let active = 'README.md';
+  let navigation = ['README.md', ':)Kudos']
   export let map;
-	// export let maker;
+  let kudosCount = 0;
 	
 	onMount(() => {
     // google analytics
