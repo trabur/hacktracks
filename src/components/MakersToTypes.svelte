60e2893899e1d286691eedb161eaec2ae3647ffe
trabur <travis.burandt@gmal.com>
2020-03-27 02:55:40 -0500

    makersToTypes

--- src/components/MakersToTypes.svelte
+++ src/components/MakersToTypes.svelte
@@ -1,15 +1,16 @@
 
 <div class="mobile only tablet only" style='padding: 0 1em; margin: 0em auto 5em; border-top: 3px solid #111;'>
-  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/library' variant="unelevated" color='secondary'>THE LIBRARY</Button>
+  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href={`/stacks/${npmPackage.config.featuredStack}`} variant="unelevated" color='secondary'>FEATURED STACK</Button>
   <br />
   <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/race-gametypes' variant="unelevated" color='secondary'>RACE GAMETYPES</Button>
 </div>
 
 <div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 0em auto 5em; border-top: 3px solid #111;'>
-  <Button style="margin: 1em 0; height: 3.6em;" href='/library' variant="unelevated" color='secondary'>THE LIBRARY</Button>
+  <Button style="margin: 1em 0; height: 3.6em;" href={`/stacks/${npmPackage.config.featuredStack}`} variant="unelevated" color='secondary'>FEATURED STACK</Button>
   <Button style="float: right; margin: 1em 0; height: 3.6em;" href='/race-gametypes' variant="unelevated" color='secondary'>RACE GAMETYPES</Button>
 </div>
 
 <script>
   import Button, {Group, GroupItem, Icon} from '@smui/button';
+	import npmPackage from '../../package.json'
 </script>
