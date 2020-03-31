5684c074ec64a69a1417cb3a5fcce97aa667caff
trabur <travis.burandt@gmal.com>
2020-03-31 03:18:57 -0500

    #

--- src/components/HACKTRACKS/HTML.svelte
+++ src/components/HACKTRACKS/HTML.svelte
@@ -3,11 +3,11 @@
   <h2 style="color: #ccc; font-size: 1em; font-weight: 500; margin: 0;">From Ideas. To Checkpoints. With Forge.</h2>
   <p style="color: #ccc; margin: -1em 0;">GOTO: the official HACKTRACKS Map Library for Halo 3 and Halo Reach maps.</p>
   <br />
-  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/stacks/bf8iU08_3BHOO2jsoe_C4KI7J-XlZG2mdKL1WToZpaw.M6_bg5-byWC_OQQXwVWE51MY79pjVPIYGjUBnmIk314' variant="unelevated" color='secondary'>#MAP LIBRARY</Button>
+  <Button style="width: 100%; margin: 1em 0; height: 3.6em;" href='/stacks/bf8iU08_3BHOO2jsoe_C4KI7J-XlZG2mdKL1WToZpaw.M6_bg5-byWC_OQQXwVWE51MY79pjVPIYGjUBnmIk314' variant="unelevated" color='secondary'>MAP LIBRARY</Button>
 </div>
 
 <div class="mobile hidden tablet hidden" style='max-width: 900px; margin: 0em auto 5em; border-top: 3px solid #111;'>
-  <Button style="float: right; margin: 1em 0; height: 3.6em;" href='/stacks/bf8iU08_3BHOO2jsoe_C4KI7J-XlZG2mdKL1WToZpaw.M6_bg5-byWC_OQQXwVWE51MY79pjVPIYGjUBnmIk314' variant="unelevated" color='secondary'>#MAP LIBRARY</Button>
+  <Button style="float: right; margin: 1em 0; height: 3.6em;" href='/stacks/bf8iU08_3BHOO2jsoe_C4KI7J-XlZG2mdKL1WToZpaw.M6_bg5-byWC_OQQXwVWE51MY79pjVPIYGjUBnmIk314' variant="unelevated" color='secondary'>MAP LIBRARY</Button>
   <h2 style="color: #ccc; font-size: 1.5em; font-weight: 500; margin: 0;">From Ideas. To Checkpoints. With Forge.</h2>
   <p style="color: #ccc; margin: -1em 0;">GOTO: the official HACKTRACKS Map Library for Halo 3 and Halo Reach maps.</p>
 </div>
