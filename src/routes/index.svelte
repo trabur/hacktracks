f062d3dcba9dc9afb53e74c5f87d94c5c8956150
trabur <travis.burandt@gmal.com>
2020-03-31 03:01:27 -0500

    HTML

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,10 +5,11 @@
 <div style="height: 100%; width: 100%;">
 	<div style="box-shadow: 0 2px 4px -1px rgba(0,0,0,.2), 0 4px 5px 0 rgba(0,0,0,.14), 0 1px 10px 0 rgba(0,0,0,.12); background: url('/reach.jpg'); height: 500px; background-position: 50% 50%; background-size: cover;"></div>
 	<div style="max-width: 900px; margin: 2em auto 5em;">
-		<h1 style="font-weight: 700; margin: 0.5em 0; text-shadow: 0 0 0.4em #000; color: #eee; font-size: 2em; padding: 0 0.5em; line-height: 1em; text-align: left; text-transform: none;">
+		<!-- <h1 style="font-weight: 700; margin: 0.5em 0; text-shadow: 0 0 0.4em #000; color: #eee; font-size: 2em; padding: 0 0.5em; line-height: 1em; text-align: left; text-transform: none;">
 			From Ideas. To Checkpoints. With Forge.
-		</h1>
+		</h1> -->
 		<Options />
+		<HTML />
 		<Favorites />
 		<div style="margin: 1em 0; display: flex;">
 			<MostKudos />
@@ -27,6 +28,7 @@
 	import {mdiConsoleLine} from '@mdi/js';
   import Button, {Group, GroupItem} from '@smui/button';
 	import ReadyToTry from '../components/ReadyToTry';
+	import HTML from '../components/HACKTRACKS/HTML';
 	import Options from '../components/HACKTRACKS/Options';
 	import Favorites from '../components/HACKTRACKS/Favorites';
 	import MostKudos from '../components/HACKTRACKS/MostKudos';
